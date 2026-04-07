a8045e7373670c51feec532d2b14bd780b73fe2e
Yuk1210 <daffyangel1210@gmail.com>
2026-04-07 14:49:01 +0200

    Add files via upload

---
+++ src/test/java/com/example/Onskeskyen/OnskeskyenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Onskeskyen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnskeskyenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
