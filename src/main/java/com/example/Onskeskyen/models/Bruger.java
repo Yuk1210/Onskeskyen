ff4e03add46f5f1c083842211bb079aaa0122c09
SaddamSatoshiQassim <saddamqassimsq97@gmail.com>
2026-04-10 12:57:51 +0200

    første entity oprettet

--- src/main/java/com/example/Onskeskyen/models/Bruger.java
+++ src/main/java/com/example/Onskeskyen/models/Bruger.java
@@ -1,4 +1,70 @@
 package com.example.Onskeskyen.models;
 
+import java.time.LocalDateTime;
+
 public class Bruger {
+    private int brugerId;
+    private String navn;
+    private String email;
+    private String kodeord;
+    private LocalDateTime oprettetDato;
+
+
+    public Bruger(int brugerId, String navn, String email, String kodeord, LocalDateTime oprettetDato){
+        this.brugerId=brugerId;
+        this.navn = navn;
+        this.email = email;
+        this.kodeord= kodeord;
+        this.oprettetDato = oprettetDato;
+    }
+
+    public int getBrugerId() {
+        return brugerId;
+    }
+
+    public void setBrugerId(int brugerId) {
+        this.brugerId = brugerId;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getKodeord() {
+        return kodeord;
+    }
+
+    public void setKodeord(String kodeord) {
+        this.kodeord = kodeord;
+    }
+
+    public LocalDateTime getOprettetDato() {
+        return oprettetDato;
+    }
+
+    public void setOprettetDato(LocalDateTime oprettetDato) {
+        this.oprettetDato = oprettetDato;
+    }
+
+    @Override
+    public String toString() {
+        return "Bruger" +
+                "brugerId" + brugerId +
+                ", navn" + navn + '\'' +
+                ", email" + email +
+                ", kodeord " + kodeord +
+                ", oprettetDato " + oprettetDato;
+    }
 }
