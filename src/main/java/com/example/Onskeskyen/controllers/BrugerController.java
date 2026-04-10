40f6b2d977001a760cfe6b722ab3aa15766027ab
ImraneRabhi801 <imranerabhi10@gmail.com>
2026-04-10 13:12:31 +0200

    joe bi

--- src/main/java/com/example/Onskeskyen/controllers/BrugerController.java
+++ src/main/java/com/example/Onskeskyen/controllers/BrugerController.java
@@ -1,4 +1,16 @@
 package com.example.Onskeskyen.controllers;
 
+import com.example.Onskeskyen.services.BrugerService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class BrugerController {
+  private final BrugerService services = new BrugerService();
+
+  @GetMapping("/")
+    public String startside(){
+          return "index";
+  }
 }
