00d3f8c7544ae6db0090737dbd17e6845d7c9b42
SaddamSatoshiQassim <saddamqassimsq97@gmail.com>
2026-04-13 13:41:03 +0200

    tilføjede en feature til repoen

--- src/main/java/com/example/Onskeskyen/repositorys/BrugerRepository.java
+++ src/main/java/com/example/Onskeskyen/repositorys/BrugerRepository.java
@@ -1,6 +1,7 @@
 package com.example.Onskeskyen.repositorys;
 
 import com.example.Onskeskyen.models.Bruger;
+import org.hibernate.tool.schema.spi.SqlScriptException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
@@ -50,33 +51,61 @@ public class BrugerRepository {
 
         return brugere;
     }
-    public Optional<Bruger> findById(int id){
-        String sql= "SELECT * FROM bruger WHERE bruger_id = ?";
+
+    public Optional<Bruger> findById(int id) {
+        String sql = "SELECT * FROM bruger WHERE bruger_id = ?";
         try (Connection connection = DriverManager.getConnection(dbUrl, username, password);
-             PreparedStatement statement = connection.prepareStatement(sql)){
+             PreparedStatement statement = connection.prepareStatement(sql)) {
 
-             statement.setInt(1,id);
-             ResultSet resultSet = statement.executeQuery();
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
 
-             if(resultSet.next()){
-                 Timestamp ts = resultSet.getTimestamp("dato");
-                 LocalDateTime dato = null;
+            if (resultSet.next()) {
+                Timestamp ts = resultSet.getTimestamp("dato");
+                LocalDateTime dato = null;
 
-                 if(ts != null){
-                     dato=ts.toLocalDateTime();
-                 }
-                 Bruger bruger = new Bruger(
-                         resultSet.getInt("bruger_id"),
-                         resultSet.getString("navn"),
-                         resultSet.getString("email"),
-                         resultSet.getString("kodeord"),
-                         dato
-                 );
-                 return Optional.of(bruger);
-             }
-        } catch (SQLException e){
-                 e.printStackTrace();
+                if (ts != null) {
+                    dato = ts.toLocalDateTime();
+                }
+                Bruger bruger = new Bruger(
+                        resultSet.getInt("bruger_id"),
+                        resultSet.getString("navn"),
+                        resultSet.getString("email"),
+                        resultSet.getString("kodeord"),
+                        dato
+                );
+                return Optional.of(bruger);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return Optional.empty();
     }
+    public void updateById(int id, String navn, String email, String kodeord){
+        String sql = "UPDATE bruger SET navn =?, email=?, kodeord=? WHERE bruger_id = ?";
+        try (Connection connection = DriverManager.getConnection(dbUrl, username, password);
+             PreparedStatement statement = connection.prepareStatement(sql)){
+
+            statement.setString(1,navn);
+            statement.setString(2,email);
+            statement.setString(3,kodeord);
+            statement.setInt(4,id);
+
+            statement.executeUpdate();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+}
+    public void deleteById(int id){
+        String sql = "DELETE FROM bruger WHERE bruger_id = ?";
+        try (Connection connection = DriverManager.getConnection(dbUrl, username, password);
+             PreparedStatement statement = connection.prepareStatement(sql)){
+            statement.setInt(1,id);
+            statement.executeUpdate();
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
 }
